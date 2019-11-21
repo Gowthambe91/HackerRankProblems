f6fcfa8cd4861b7487e10e32037a77251f77abd4
Gowtham <Gowthambe91@gmail.com>
2019-11-21 08:15:45 +0530

    Hacker Rank MinMaxSum Initial Commit - https://github.com/Gowthambe91/HackerRankProblems.git

--- MinMaxSum/Program.cs
+++ MinMaxSum/Program.cs
@@ -10,6 +10,35 @@ namespace MinMaxSum
     {
         static void Main(string[] args)
         {
+            int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            miniMaxSum(array);
+            Console.ReadLine();
+        }
+
+        static void miniMaxSum(int[] arr)
+        {
+            int minSum = 0; int maxSum = 0;
+            Int64 Sum = arr.Sum();
+            int minVal = arr.Min();
+            int maxVal = arr.Max();
+            List<int> lst = arr.ToList<int>();
+            lst.Sort();
+            
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (i != arr.Length - 1)
+            //    {
+            //        minSum += arr[i];
+            //    }
+
+            //    if (i != 0)
+            //    {
+            //        maxSum += arr[i];
+            //    }
+            //}
+
+            Console.WriteLine("{0} {1} Sum = {2}, minVal = {3}, maxVal = {4}, lstSum = {5}", Sum - maxVal, Sum - minVal, Sum, minVal
+                ,maxVal, lst.Sum());
         }
     }
 }
