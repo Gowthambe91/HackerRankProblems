466012a9b58cdf4730b347a6600642588ef3face
GowthamanJegan <gowthambe91@gmail.com>
2019-11-21 19:01:25 +0530

    AppleandOrangeCount Partial commit

---
+++ AppleandOrange_Count/AppleandOrangeCount/AppleandOrangeCount/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppleandOrangeCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Start and End Position of Sam's House with Spaces:");
+            string[] st = Console.ReadLine().Split(' ');
+            int s = Convert.ToInt32(st[0]); //start point
+            int t = Convert.ToInt32(st[1]); // end point
+
+            Console.WriteLine("\n Enter the Position of Apple and Orange tree :");
+
+        }
+    }
+}
+
